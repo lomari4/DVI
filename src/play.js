d4194c4a793ace2c8501fe944b098055086bef2b
lomari4 <60477548+lomari4@users.noreply.github.com>
2020-03-12 18:18:18 +0100

    base64 encoding

--- src/play.js
+++ src/play.js
@@ -6,6 +6,7 @@ class Play extends Phaser.Scene {
 		//cargamos el audio
 		this.load.audio("gameSound", "./assets/music/soundtrack/Snow.mp3");
 		this.load.image("bg", "./assets/backgrounds/backgroundForest_extended.png");
+
 		//cargamos el tileset
 		this.load.image('tiles', './assets/tiles/tilemap.png');
 		//cargamos las plantas
@@ -13,6 +14,7 @@ class Play extends Phaser.Scene {
 		this.load.image('bushFrozen', './assets/environment/bushFrozen.png');
 		this.load.image('treePine', './assets/environment/treePine.png');
 		this.load.image('treePineFrozen', './assets/environment/treePineFrozen.png');
+
 		//cargamos el mapa de tiled en json
 		this.load.tilemapTiledJSON('map', './assets/levels/nivel1.json');
 		this.load.image('fullscreen', './assets/fullScreen.png');
@@ -22,37 +24,36 @@ class Play extends Phaser.Scene {
 		//añadimos el sonido
 		let soundsGame = this.sound.add("gameSound");
 		soundsGame.setLoop(true);
-        soundsGame.play();
+		soundsGame.play();
+		
 		//añadimos el background
 		this.add.image(0, 0, "bg").setOrigin(0).setDepth(0);
-		
+
+		//añadimos boton de fullscreen con su funcionalidad
 		let button = this.add.image(1472-80, 16, 'fullscreen', 0).setOrigin(0).setInteractive();
-
         button.on('pointerup', function () {
-
             if (this.scale.isFullscreen)
-            {
-                this.scale.stopFullscreen();
-            }
+            	this.scale.stopFullscreen();
             else
-            {
-                this.scale.startFullscreen();
-            }
-
+            	this.scale.startFullscreen();
 		}, this);
-
-		//Lo he comentado porque no iba
-		/*
+		
 		//añadimos el mapa
-		let map = this.make.tilemap({key:'map'});
+		let map = this.make.tilemap({ 
+			key: 'map', 
+			tileWidth: 64, 
+			tileHeight: 64 
+		  });
+
 		//añadimos los tiles 64x64 al map
 		let tileset = map.addTilesetImage('tilemap', 'tiles', 64, 64);
+
 		//añadimos las capas del mapa. Asegurarse de que los args coinciden con los nombres de las layers en tiled
-		let ground = map.createStaticLayer('ground', tileset); //sera layer dinamica en un futuro
-		let plants = map.createStaticLayer('plants', plantmap); //sera layer dinamica en un futuro
+		let groundLayer = map.createStaticLayer('ground', tileset); //sera layer dinamica en un futuro
+		let plantLayer = map.createStaticLayer('plants', plantmap); //sera layer dinamica en un futuro
 		//ground.setCollisionFromCollisionGroup() //colision por grupo de tiled collision editor
 		
-       */
+       
     }
 
     update(time, delta) {}
