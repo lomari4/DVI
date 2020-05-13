fef3f2df2e6a3494ac811b11b3f34d7ddf510127
lomari4 <marinl19@ucm.es>
2020-05-13 14:09:03 +0200

    Update pausedgame.js

--- src/pausedgame.js
+++ src/pausedgame.js
@@ -2,8 +2,6 @@ export default class PausedGame extends Phaser.Scene {
 
     constructor() {
         super({ key: 'PausedGame' });
-        this.widthAdded = 150;
-        this.heightAdded = 40;
         this.heighttoPauseGame = 150;
     }
 
@@ -15,16 +13,7 @@ export default class PausedGame extends Phaser.Scene {
 
     create() {
         this.checkLevel(); //ver que nivel ha llamado al pause gmae
-
-        let menuSelect = this.sound.add("menu_select_sound", {
-            volume: 0.40,
-        });
-        let menuHover = this.sound.add("menu_hover_sound", {
-            volume: 2,
-        });
-
-        this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2 - this.heighttoPauseGame, "pause").setDepth(1);
-        
+        this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2 - this.heighttoPauseGame, "pause").setDepth(1);       
         this.key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
         let font = this.add.renderTexture(0, 0, this.game.renderer.width, this.game.renderer.height);
         font.fill(0x000000, 0.5);
